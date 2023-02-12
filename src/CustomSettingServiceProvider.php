bc9dc91826f8d4f7f27e67fe483c44809bd53fa6
Max Kalahur <kalahur@teamharbour.com>
2023-02-12 16:03:55 +0200

    Migrations fix

--- src/CustomSettingServiceProvider.php
+++ src/CustomSettingServiceProvider.php
@@ -8,6 +8,7 @@ class CustomSettingServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         $this->publishMigrations();
     }
 
