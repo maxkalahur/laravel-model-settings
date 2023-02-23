6da932b2e7d0b20caa5035de685979a0a88699e9
Max Kalahur <kalahur@teamharbour.com>
2023-02-24 00:11:35 +0200

    Test Migrations

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,8 +7,6 @@ use Orchestra\Testbench\TestCase as Orchestra;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use MaxKalahur\LaravelModelSettings\Tests\Models\DummyUser;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
 abstract class TestCase extends Orchestra
 {
@@ -17,32 +15,16 @@ abstract class TestCase extends Orchestra
     public function setUp(): void
     {
         parent::setUp();
-
-        Schema::create('test_users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email');
-            $table->timestamps();
-        });
-        
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         
         DummyUser::create([
             'name' => 'Test',
             'email' => 'test@test.com',
         ]);
     }
-    
-    public function tearDown(): void
-    {
-        parent::tearDown();
-        
-        Schema::drop('test_users');   
-        Schema::drop('custom_settings');  
-    }
 
     protected function defineDatabaseMigrations()
     {
+        $this->loadMigrationsFrom(__DIR__.'/migrations');
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
     }
 
