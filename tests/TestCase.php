271465eab2557966907a72ac45607820e4dd1c8a
Max Kalahur <kalahur@teamharbour.com>
2025-07-30 11:20:17 +0200

    Tests update

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,14 +2,11 @@
 
 namespace MaxKalahur\LaravelModelSettings\Tests;
 
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 use MaxKalahur\LaravelModelSettings\CustomSettingServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 abstract class TestCase extends Orchestra
 {
-    use DatabaseTransactions;
-
     protected function defineDatabaseMigrations()
     {
         $this->loadMigrationsFrom(__DIR__.'/migrations');
