66aa1087d74efd747087f010fe3f2bc07f438495
Max Kalahur <kalahur@teamharbour.com>
2025-08-01 22:55:31 +0200

    Migration update

--- database/migrations/2018_08_08_100000_create_custom_settings_table.php
+++ database/migrations/2018_08_08_100000_create_custom_settings_table.php
@@ -22,6 +22,8 @@ class CreateCustomSettingsTable extends Migration
             $table->mediumText('value')->nullable();
             $table->boolean('is_encrypted')->default(false);
             $table->timestamps();
+
+            $table->index(['model_type', 'model_id']);
         });
     }
 
